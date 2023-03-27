69968253abba37fb0c3cf164cce0f8c0bfc57b2a
Sagar Mittal <mittalsagar006@gmail.com>
2023-03-27 23:54:47 +0530

    made fetch function to send request to openAI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import { Container, Box } from '@chakra-ui/react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import TextInput from './components/TextInput';
 
 const App = () => {
-  const extractKeywords = (text) => {
-    console.log(text);
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract keywords from this text. Make the first letter of each word uppercase and separate with comma\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8,
+      }),
+    };
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+    const json = await response.json();
+
+    const data = json.choices[0].text.trim();
+    console.log(data);
+
+    setKeywords(data);
+    setLoading(false);
   };
 
   return (
